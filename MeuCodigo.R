8073384cb9673dcda78f77c993f928dd33a0d1d8
HerickSilva9 <149031628+HerickSilva9@users.noreply.github.com>
2023-11-21 00:45:14 -0300

    Começo do esboço do código

--- MeuCodigo.R
+++ MeuCodigo.R
@@ -1,45 +1,54 @@
-# Professor: Cassius Henrique
-
-# Aula 22 - Exemplo 1
 # Modelo de regressao linear simples
 
 rm(list=ls(all=TRUE))
 
-require(tidyverse)
-require(plotly)
-require(lmtest)
-require(ggfortify)
-require(broom)
-require(ggExtra)
+if(!require(pacman)) install.packages("pacman")
+library(pacman)
+
+pacman::p_load(tidyverse, plotly, lmtest, ggfortify, broom, ggExtra, car)
+
 
 # Dados
-dt <- read.csv("~/EstR/est002/dados/GastosReceitas.csv",
+dt <- read.csv("~/EstR/est057/dados/SalarioExperiencia.csv",
                header = TRUE, sep = ";", dec = ".")
+
+glimpse(dt)
+
+#Definição da notação do p-valor
+options(scipen=999)
+
+# Retirando linhas da base de dados (apenas para curiosidade)
+# dt <- dt[-18,]
 
 
 # Estatisticas descritivas
-summary(dt$Gastos)
-summary(dt$Receitas)
-nrow(dt)
-cor(dt$Receitas, dt$Gastos)
+summary(dt$salario)
+summary(dt$experiencia)
+cor(dt$experiencia, dt$salario)
 
 
-resposta <- ggplot(dt, aes(x = Gastos))+
+
+resposta <- ggplot(dt, aes(x = experiencia))+
   geom_histogram(aes(y = after_stat(count / sum(count))),
                  bins = 7, fill="blue")+
   scale_y_continuous(labels = scales::percent)+
   labs(y="FR",
-       x="Gastos (em R$1000,00)",
+       x="experiencia (em R$1000,00)",
        title="Histograma")+
   theme(text = element_text(size = 12))
 ggplotly(resposta)
 
-disp <- ggplot(data = dt, aes(x = Receitas, y = Gastos)) +
+disp <- ggplot(data = dt, aes(x = experiencia, y = salario)) +
   geom_point(color='blue') +
-  ggtitle("Grafico de dispersao e boxplots marginais") +
-  geom_count() +
+  ggtitle("Grafico de dispersao") +
   geom_smooth(method = "lm", se = TRUE)
 ggplotly(disp)
+
+disp2 <- ggplot(data = dt, aes(x = experiencia, y = salario)) +
+  geom_point(color='blue') +
+  ggtitle("Grafico de dispersao e boxplots marginais") +
+  geom_smooth(method = "lm", se = TRUE)
+ggplotly(disp2)
 
 ggMarginal(disp,
            type = "boxplot",
@@ -47,11 +56,12 @@ ggMarginal(disp,
 
 
 # Teste de correlacao
-cor.test(dt$Receitas, dt$Gastos)
+cor.test(dt$salario, dt$experiencia)
 
 
 # Regressao linear
-modelo <- lm(Gastos ~ Receitas, data = dt)
+# y ~ x (y = resposta, x = explicativa)
+modelo <- lm(salario ~ experiencia, data = dt)
 summary(modelo)
 confint(modelo)
 
@@ -61,12 +71,15 @@ confint(modelo)
 # pontos mais influentes
 autoplot(modelo)
 
+
 # Outra forma
+plot(residuals(modelo))
 plot(modelo,1)
 plot(modelo,2)
 plot(modelo,3)
 plot(modelo,4)
 plot(modelo,5)
+
 
 res <- residuals(modelo) %>%
   as.data.frame()
@@ -93,12 +106,10 @@ ggMarginal(ggres,
 
 model.diag.metrics <- augment(modelo)
 head(model.diag.metrics)
-ggplot(model.diag.metrics, aes(Receitas, Gastos)) +
+ggplot(model.diag.metrics, aes(experiencia, salario)) +
   geom_point() +
   stat_smooth(method = lm, se = FALSE) +
-  geom_segment(aes(xend = Receitas, yend = .fitted), color = "red", size = 0.3) +
-  geom_count() +
-  geom_smooth(method = "lm", se = TRUE) +
+  geom_segment(aes(xend = experiencia, yend = .fitted), color = "red", size = 0.3) +
   ggtitle("Residuos do modelo")
 
 
@@ -106,133 +117,21 @@ ggplot(model.diag.metrics, aes(Receitas, Gastos)) +
 summary(aov(modelo))
 
 # Normalidade
+# H0: residuos normais
+# H1: residuos nao normais
 shapiro.test( residuals(modelo) )
 
-# Homocedasticidade (aqui e possivel usar esse teste somente porque
-# a variavel explicativa esta disposta em grupos)
-bartlett.test( Gastos ~ Receitas, data = dt)
 
 # Realizar o teste de Breusch-Pagan
+# H0: residuos homocedasticos
+# H1: residuos heteroscedasticos
 bptest(modelo)
 
-# Professor: Cassius Henrique
-
-# Aula 22 - Exemplo 1
-# Modelo de regressao linear simples
-
-rm(list=ls(all=TRUE))
-
-require(tidyverse)
-require(plotly)
-require(lmtest)
-require(ggfortify)
-require(broom)
-require(ggExtra)
-
-# Dados
-dt <- read.csv("~/EstR/est002/dados/GastosReceitas.csv",
-               header = TRUE, sep = ";", dec = ".")
+# Teste de Durbin-Watson
+# H0: autocorrelacao de residuos igual a zero
+# H1: autocorrelacao de residuos maior zero
+dwtest(modelo)
 
 
-# Estatisticas descritivas
-summary(dt$Gastos)
-summary(dt$Receitas)
-nrow(dt)
-cor(dt$Receitas, dt$Gastos)
+# gvlma::gvlma(modelo)
 
-
-resposta <- ggplot(dt, aes(x = Gastos))+
-  geom_histogram(aes(y = after_stat(count / sum(count))),
-                 bins = 7, fill="blue")+
-  scale_y_continuous(labels = scales::percent)+
-  labs(y="FR",
-       x="Gastos (em R$1000,00)",
-       title="Histograma")+
-  theme(text = element_text(size = 12))
-ggplotly(resposta)
-
-disp <- ggplot(data = dt, aes(x = Receitas, y = Gastos)) +
-  geom_point(color='blue') +
-  ggtitle("Grafico de dispersao e boxplots marginais") +
-  geom_count() +
-  geom_smooth(method = "lm", se = TRUE)
-ggplotly(disp)
-
-ggMarginal(disp,
-           type = "boxplot",
-           fill = "lightblue")
-
-
-# Teste de correlacao
-cor.test(dt$Receitas, dt$Gastos)
-
-
-# Regressao linear
-modelo <- lm(Gastos ~ Receitas, data = dt)
-summary(modelo)
-confint(modelo)
-
-# Graficos para analise do modelo:
-# Aqui sera feita a analise da suposicao de independencia, de
-# normalidade e de homocedasticidade. Tambem e possivel avaliar os
-# pontos mais influentes
-autoplot(modelo)
-
-# Outra forma
-plot(modelo,1)
-plot(modelo,2)
-plot(modelo,3)
-plot(modelo,4)
-plot(modelo,5)
-
-res <- residuals(modelo) %>%
-  as.data.frame()
-names(res) <- "residuos"
-res$x <- as.numeric(rownames(res))
-ggres <- res %>%
-  ggplot(aes(x = x, y = residuos)) +
-  geom_point(color='blue', size = 3) +
-  ggtitle("Residuos do modelo") +
-  geom_hline(aes(yintercept = 0, colour = "red"), size=1) +
-  guides(color = FALSE, size = FALSE)
-
-ggMarginal(ggres,
-           type = "boxplot",
-           fill = "lightblue",
-           margins = "y")
-
-ggMarginal(ggres,
-           type = "histogram",
-           bins = ceiling(sqrt(nrow(res)))+2,
-           fill = "lightblue",
-           margins = "y")
-
-
-model.diag.metrics <- augment(modelo)
-head(model.diag.metrics)
-ggplot(model.diag.metrics, aes(Receitas, Gastos)) +
-  geom_point() +
-  stat_smooth(method = lm, se = FALSE) +
-  geom_segment(aes(xend = Receitas, yend = .fitted), color = "red", size = 0.3) +
-  geom_count() +
-  geom_smooth(method = "lm", se = TRUE) +
-  ggtitle("Residuos do modelo")
-
-
-# ANOVA
-summary(aov(modelo))
-
-# Normalidade
-shapiro.test( residuals(modelo) )
-
-# Homocedasticidade (aqui e possivel usar esse teste somente porque
-# a variavel explicativa esta disposta em grupos)
-bartlett.test( Gastos ~ Receitas, data = dt)
-
-# Realizar o teste de Breusch-Pagan
-bptest(modelo)
-
-#git status
-#git add .
-#git commit -m ""
-#git push origin main
